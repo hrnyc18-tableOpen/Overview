bd3a36f53c9cac8984acb18552a784fcd73a3848
mfosho <mc8160@gmail.com>
2018-11-07 11:19:54 -0500

    cleaned up code

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const autoIncrement = require('mongoose-auto-increment');
 
-mongoose.connect('mongodb://maria:0237100t@ds147073.mlab.com:47073/tableopen') //hosted version from mlab
+mongoose.connect('mongodb:maria:0237100t@ds147073.mlab.com:47073/tableopen') hosted version from mlab
 
 const db = mongoose.connection;
 autoIncrement.initialize(db);
@@ -11,41 +11,41 @@ const Schema = mongoose.Schema;
 mongoose.set('useCreateIndex', true)
 
 const OverviewChildSchema = new Schema({
-  name: {type: String, required: true}, //
-  review_count: Number, //
-  display_address: Array, //
-  display_phone: String, //
-  coordinates: Object, //
-  website: String //
+  name: {type: String, required: true}, 
+  review_count: Number, 
+  display_address: Array,
+  display_phone: String, 
+  coordinates: Object, 
+  website: String 
 })
 
 const OverviewSchema = new Schema({
   rid: {type: Number, required: true},
-  OverviewChildSchema, //
-  aggregate_score: Number, //
-  price_quantile: String, //
-  cuisine: Array, //
-  tags: Array, // 
-  description: String, // 
-  private_dining: Boolean, // 
-  dining_style: String, //
-  hours: Array, //
-  payment_options: Array, // 
-  dress_code: String, //
-  executive_chef: String, //
-  catering: String, //
-  private_party_facilities: String, //
-  private_party_contact_name: String, //
-  private_party_contact_number: String, //
-  coordinates: Object, // 
-  neighborhood: String, //
-  cross_street: String, //
-  parking_details: String, //
-  public_transit: String, //
-  entertainment: String, //
-  special_events_promotions: String, //
-  additional: String, //
-  private_dining_details: String //
+  OverviewChildSchema, 
+  aggregate_score: Number, 
+  price_quantile: String, 
+  cuisine: Array, 
+  tags: Array,  
+  description: String,  
+  private_dining: Boolean,  
+  dining_style: String, 
+  hours: Array, 
+  payment_options: Array,  
+  dress_code: String, 
+  executive_chef: String, 
+  catering: String, 
+  private_party_facilities: String, 
+  private_party_contact_name: String, 
+  private_party_contact_number: String, 
+  coordinates: Object,  
+  neighborhood: String, 
+  cross_street: String, 
+  parking_details: String, 
+  public_transit: String, 
+  entertainment: String, 
+  special_events_promotions: String, 
+  additional: String, 
+  private_dining_details: String 
 })
 
 OverviewSchema.plugin(autoIncrement.plugin, {model: 'Overview', field: 'rid', startAt: 1, incrementBy: 1})
--- server/controllers/overviewController.js
+++ server/controllers/overviewController.js
@@ -7,7 +7,6 @@ module.exports = {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   },
   getRestaurant: (req, res) => {
-    console.log('req params', req.params.id)
     model.get(req.params.id, (data) => {
       res.send(data);
     })
